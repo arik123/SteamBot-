aad60b3ff5b8a0ef53d38e9290ccef37507fc355
Max <maliarmaximilian@gmail.com>
2021-08-02 20:55:11 +0200

    before refactor

--- WebRequest.cpp
+++ WebRequest.cpp
@@ -16,10 +16,10 @@ void WebRequest::on_read(beast::error_code ec, std::size_t bytes_transferred) {
     callback(res_);
 
     // Set a timeout on the operation
-    beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+    beast::get_lowest_layer(sslStream_).expires_after(std::chrono::seconds(30));
 
     // Gracefully close the stream
-    stream_.async_shutdown([&](beast::error_code ec){on_shutdown(ec);});
+    sslStream_.async_shutdown([&](beast::error_code ec){on_shutdown(ec);});
 }
 
 void WebRequest::on_shutdown(beast::error_code ec) {
@@ -32,7 +32,7 @@ void WebRequest::on_shutdown(beast::error_code ec) {
         return fail(ec, "shutdown");
 
     // If we get here then the connection is closed gracefully
-    shutdown_cb();
+    shutdown_cb(this);
 }
 
 void WebRequest::on_write(beast::error_code ec, std::size_t bytes_transferred) {
@@ -42,7 +42,7 @@ void WebRequest::on_write(beast::error_code ec, std::size_t bytes_transferred) {
         return fail(ec, "write");
 
     // Receive the HTTP response
-    http::async_read(stream_, buffer_, res_, [&](beast::error_code ec, std::size_t bt){on_read(ec, bt);});
+    http::async_read(sslStream_, buffer_, res_, [&](beast::error_code ec, std::size_t bt){on_read(ec, bt);});
 }
 
 void WebRequest::on_handshake(beast::error_code ec) {
@@ -50,10 +50,10 @@ void WebRequest::on_handshake(beast::error_code ec) {
         return fail(ec, "handshake");
 
     // Set a timeout on the operation
-    beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+    beast::get_lowest_layer(sslStream_).expires_after(std::chrono::seconds(30));
 
     // Send the HTTP request to the remote host
-    http::async_write(stream_, req_, [&](beast::error_code ec, std::size_t bt){on_write(ec, bt);});
+    http::async_write(sslStream_, req_, [&](beast::error_code ec, std::size_t bt){on_write(ec, bt);});
 }
 
 void WebRequest::on_connect(beast::error_code ec, const asio::ip::basic_endpoint<asio::ip::tcp> &) {
@@ -61,31 +61,37 @@ void WebRequest::on_connect(beast::error_code ec, const asio::ip::basic_endpoint
         return fail(ec, "connect");
 
     // Perform the SSL handshake
-    stream_.async_handshake(ssl::stream_base::client,[&](beast::error_code ec){on_handshake(ec);});
+    if(ssl) sslStream_.async_handshake(ssl::stream_base::client, [&](beast::error_code ec){on_handshake(ec);});
+    else on_handshake(ec);
 }
 
 void WebRequest::on_resolve(beast::error_code ec,
                             const asio::ip::basic_resolver<asio::ip::tcp, asio::any_io_executor>::results_type &results) {
     if (ec)
         return fail(ec, "resolve");
-
-    if (!SSL_set_tlsext_host_name(stream_.native_handle(), host.c_str())) {
-        beast::error_code ec{static_cast<int>(::ERR_get_error()), asio::error::get_ssl_category()};
-        std::cerr << ec.message() << "\n";
-        return;
+    if(ssl){
+        if (!SSL_set_tlsext_host_name(sslStream_.native_handle(), host.c_str())) {
+            beast::error_code ec2{static_cast<int>(::ERR_get_error()), asio::error::get_ssl_category()};
+            std::cerr << ec2.message() << "\n";
+            return;
+        }
+        usedStream_ = &beast::get_lowest_layer(sslStream_);
+    } else {
+        usedStream_ = &stream_;
     }
     // Set a timeout on the operation
-    beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+    beast::get_lowest_layer(sslStream_).expires_after(std::chrono::seconds(30));
 
     // Make the connection on the IP address we get from a lookup
-    beast::get_lowest_layer(stream_).async_connect(results,
-                                                   [&](beast::error_code ec, const net::resolver::results_type::endpoint_type& et){on_connect(ec, et);});
+    beast::get_lowest_layer(sslStream_).async_connect(results,
+                                                      [&](beast::error_code ec, const net::resolver::results_type::endpoint_type& et){on_connect(ec, et);});
 }
 
 WebRequest::WebRequest(asio::any_io_executor ex, ssl::context &ctx, std::string host, std::string endpoint,
                        http::request<http::string_body> req,
-                       const std::function<void(http::response<http::string_body>&)> callback,
-                       std::function<void()> shutdown_cb)
-        : stream_(ex, ctx), host(std::move(host)), endpoint(std::move(endpoint)), callback(std::move(callback)), shutdown_cb(std::move(shutdown_cb)){
+                       std::function<void(http::response<http::string_body>&)>  callback,
+                       std::function<void(WebRequest*)> shutdown_cb,
+                       bool ssl)
+        : sslStream_(ex, ctx), stream_(ex), host(std::move(host)), endpoint(std::move(endpoint)), callback(std::move(callback)), shutdown_cb(std::move(shutdown_cb)), ssl(ssl){
     req_ = std::move(req);
 }
--- WebRequest.h
+++ WebRequest.h
@@ -22,15 +22,18 @@ namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
 using net = boost::asio::ip::tcp;    // from <boost/asio.hpp>
 struct WebRequest {
+    bool ssl;
     http::request<http::string_body> req_;
     http::response<http::string_body> res_;
-    beast::ssl_stream<beast::tcp_stream> stream_;
+    beast::tcp_stream stream_;
+    beast::tcp_stream * usedStream_;
+    beast::ssl_stream<beast::tcp_stream> sslStream_;
     beast::flat_buffer buffer_; // (Must persist between reads)
     const std::string host;
     std::string endpoint;
     const std::function<void(http::response<http::string_body>&)> callback;
-    std::function<void()> shutdown_cb;
-    WebRequest(asio::any_io_executor ex, ssl::context& ctx, std::string host, std::string endpoint, http::request<http::string_body> req, std::function<void(http::response<http::string_body>&)>  callback, std::function<void()> shutdown_cb);
+    std::function<void(WebRequest*)> shutdown_cb;
+    WebRequest(asio::any_io_executor ex, ssl::context& ctx, std::string host, std::string endpoint, http::request<http::string_body> req, std::function<void(http::response<http::string_body>&)>   callback, std::function<void(WebRequest*)> shutdown_cb, bool ssl = true);
 
     void on_resolve(beast::error_code ec, const net::resolver::results_type& results);
 
